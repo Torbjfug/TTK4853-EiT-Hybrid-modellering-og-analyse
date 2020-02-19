385a6621f785e3e0fb1994b5d168e91d960376c3
Torbjfug <torbjfug@stud.ntnu.no>
2020-02-19 08:54:32 +0100

    Split filehandling and means calculations

---
+++ python/fileHandling.py
@@ -0,0 +1,50 @@
+import h5py
+import os
+import numpy as np
+import hdf5storage
+from tqdm import tqdm
+
+def save_hdf5(data, filename):
+    with h5py.File(filename, 'w') as data_file:
+        for key in data.keys():
+            data_file.create_dataset(key, data=data[key],)
+
+
+def load_hdf5(filename):
+    with h5py.File(filename, 'r') as f:
+        data = {}
+        for key in list(f.keys()):
+            data[key] = np.array(f[key])
+    return data
+
+
+def concatinate_files(data1, data2):
+    for key in data2.keys():
+        data1[key] = np.concatenate((data1[key], data2[key]))
+    return data1
+
+
+
+
+
+def concatenate_hdf5(filename, data):
+    with h5py.File(filename, 'a') as f:
+        dataset_keys = list(f.keys())
+        for key in data.keys():
+            if key in dataset_keys:
+                dset = f[key]
+                dset.resize(dset.shape[0]+data[key].shape[0], axis=0)
+                dset[-data[key].shape[0]:] = data[key]
+            else:
+                max_size = (None,)+data[key].shape[1:]
+                f.create_dataset(key, data=data[key], maxshape=max_size)
+    return data
+
+
+def concatenate_files_in_folder(source_dir, save_file):
+    for filename in tqdm(os.listdir(source_dir)):
+        if filename.endswith('.mat'):
+            data = hdf5storage.loadmat(source_dir + filename)
+            concatenate_hdf5(save_file, data)
+    all_data = load_hdf5(save_file)
+    return all_data
--- python/find_norm.py
+++ python/find_norm.py
@@ -4,49 +4,8 @@ import hdf5storage
 import pickle
 import h5py
 from tqdm import tqdm
+from fileHandling import *
 
-
-def save_hdf5(data, filename):
-    with h5py.File(filename, 'w') as data_file:
-        for key in data.keys():
-            data_file.create_dataset(key, data=data[key],)
-
-
-def load_hdf5(filename):
-    with h5py.File(filename, 'r') as f:
-        data = {}
-        for key in list(f.keys()):
-            data[key] = np.array(f[key])
-    return data
-
-
-def concatinate_files(data1, data2):
-    for key in data2.keys():
-        data1[key] = np.concatenate((data1[key], data2[key]))
-    return data1
-
-
-def concatenate_hdf5(filename, data):
-    with h5py.File(filename, 'a') as f:
-        dataset_keys = list(f.keys())
-        for key in data.keys():
-            if key in dataset_keys:
-                dset = f[key]
-                dset.resize(dset.shape[0]+data[key].shape[0], axis=0)
-                dset[-data[key].shape[0]:] = data[key]
-            else:
-                max_size = (None,)+data[key].shape[1:]
-                f.create_dataset(key, data=data[key], maxshape=max_size)
-    return data
-
-
-def concatenate_files_in_folder(source_dir, save_file):
-    for filename in tqdm(os.listdir(source_dir)):
-        if filename.endswith('.mat'):
-            data = hdf5storage.loadmat(source_dir + filename)
-            concatenate_hdf5(save_file, data)
-    all_data = load_hdf5(save_file)
-    return all_data
 
 
 def calculate_means(data):
@@ -103,8 +62,8 @@ if __name__ == "__main__":
         save_hdf5(means, 'means.hdf5')
         save_hdf5(stds, 'stds.hdf5')
 
-    test_file = 'data/calibration/2017_11_05.mat'
-    test_data = load_hdf5(test_file)
+    #test_file = 'data/calibration/2017_11_05.mat'
+    #test_data = load_hdf5(test_file)
     test_data_norm = normalize_dataset(test_data, means, stds)
     test_mean = calculate_means(test_data_norm)
     test_std = calculate_std(test_data_norm)
