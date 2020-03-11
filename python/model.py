f0bf707da5661e1ffd6c3e1cc6a27d87c62de5bc
torbjfug <torbjfug@stud.ntnu.no>
2020-03-11 12:17:54 +0100

    Add batch normalization

--- python/model.py
+++ python/model.py
@@ -44,64 +44,86 @@ class Model(nn.Module):
         self.kernels = [kernel_dim, kernel_dim, kernel_dim]
         self.encoded = None
 
-        self.conv1 = nn.Conv3d(
-            in_channels=image_channels,
-            out_channels=self.num_filters[0],
-            kernel_size=3,
-            padding=1)
+        self.conv1 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=image_channels,
+                out_channels=self.num_filters[0],
+                kernel_size=3,
+                padding=1),
+            nn.BatchNorm3d(num_features=self.num_filters[0]),
+            nn.ReLU(),
+        )
 
-        self.conv2 = nn.Conv3d(
-            in_channels=self.num_filters[0],
-            out_channels=self.num_filters[1],
-            kernel_size=3,
-            padding=1)
+        self.conv2 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=self.num_filters[0],
+                out_channels=self.num_filters[1],
+                kernel_size=3,
+                padding=1),
+            nn.BatchNorm3d(num_features=self.num_filters[1]),
+            nn.ReLU(),
+        )
 
-        self.conv3 = nn.Conv3d(
-            in_channels=self.num_filters[1],
-            out_channels=self.num_filters[2],
-            kernel_size=3,
-            padding=1)
+        self.conv3 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=self.num_filters[1],
+                out_channels=self.num_filters[2],
+                kernel_size=3,
+                padding=1),
+            nn.BatchNorm3d(num_features=self.num_filters[2]),
+            # nn.ReLU(),
+        )
 
         self.pool = nn.MaxPool3d(kernel_size=2, stride=2, return_indices=True)
         self.unpool = nn.MaxUnpool3d(kernel_size=2, stride=2)
 
         # decoder layers
-        self.t_conv1 = nn.ConvTranspose3d(
-            in_channels=self.num_filters[2],
-            out_channels=self.num_filters[1],
-            kernel_size=3,
-            padding=1,
-            stride=1,
+        self.t_conv1 = nn.Sequential(
+            nn.ConvTranspose3d(
+                in_channels=self.num_filters[2],
+                out_channels=self.num_filters[1],
+                kernel_size=3,
+                padding=1,
+                stride=1,
+            ),
+            nn.BatchNorm3d(num_features=self.num_filters[1]),
+            nn.ReLU(),
         )
 
-        self.t_conv2 = nn.ConvTranspose3d(
-            in_channels=self.num_filters[1],
-            out_channels=self.num_filters[0],
-            kernel_size=3,
-            padding=1,
-            stride=1
+        self.t_conv2 = nn.Sequential(
+            nn.ConvTranspose3d(
+                in_channels=self.num_filters[1],
+                out_channels=self.num_filters[0],
+                kernel_size=3,
+                padding=1,
+                stride=1,
+            ),
+            nn.BatchNorm3d(num_features=self.num_filters[0]),
+            nn.ReLU(),
         )
 
-        self.t_conv3 = nn.ConvTranspose3d(
-            in_channels=self.num_filters[0],
-            out_channels=image_channels,
-            kernel_size=3,
-            padding=1,
-            stride=1
+        self.t_conv3 = nn.Sequential(
+            nn.ConvTranspose3d(
+                in_channels=self.num_filters[0],
+                out_channels=image_channels,
+                kernel_size=3,
+                padding=1,
+                stride=1,
+            ),
         )
 
         self.pool_indecies = [(), ()]
-        self.activation = nn.ReLU(inplace=True)
+        self.activation = nn.ReLU()
 
     def encode(self, x):
         x = self.conv1(x)
-        self.activation(x)
+        # self.activation(x)
         (x, self.pool_indecies[0]) = self.pool(x)
         x = self.conv2(x)
-        self.activation(x)
+        # self.activation(x)
         (x, self.pool_indecies[1]) = self.pool(x)
         x = self.conv3(x)
-        self.activation(x)
+        # self.activation(x)
         return x
 
     def decode(self, x):
@@ -109,7 +131,7 @@ class Model(nn.Module):
         self.activation(x)
         x = self.unpool(x, self.pool_indecies[1])
         x = self.t_conv2(x)
-        self.activation(x)
+        # self.activation(x)
         x = self.unpool(x, self.pool_indecies[0])
         x = self.t_conv3(x)
         return x
@@ -126,10 +148,11 @@ class Model(nn.Module):
 
 
 if __name__ == "__main__":
+    test_name = "Add_batch_norm"
     x_dim = 16
     z_dim = 16
     batch_size = 32
-    epochs = 5
+    epochs = 1
     learning_rate = 1e-3
     early_stop_count = 4
     dataset = weatherDataSet(x_range=[0, x_dim],
@@ -139,7 +162,7 @@ if __name__ == "__main__":
     dataloader = DataLoader(dataset,
                             batch_size=batch_size,
                             shuffle=True,
-                            num_workers=4)
+                            num_workers=0)
     val_dataset = weatherDataSet(x_range=[0, x_dim],
                                  y_range=[0, x_dim],
                                  z_range=[0, z_dim],
@@ -147,7 +170,7 @@ if __name__ == "__main__":
     validation_dataloader = DataLoader(val_dataset,
                                        batch_size=64,
                                        shuffle=True,
-                                       num_workers=4)
+                                       num_workers=0)
     dataloaders = (dataloader, validation_dataloader, validation_dataloader)
     model = Model(3, [z_dim, x_dim, x_dim])
 
@@ -158,20 +181,20 @@ if __name__ == "__main__":
         epochs,
         model,
         dataloaders,
-        "test"
+        test_name
     )
 
     print(torch.cuda.is_available())
     train = False
     if train:
         trainer.train()
-        create_plots(trainer, "test")
+        create_plots(trainer, test_name)
     else:
         trainer.load_best_model()
         trainer.load_statistic('test')
         create_plots(trainer, "test2")
-    data_sample = utils.to_cuda(next(iter(validation_dataloader)))
-    #reconstructed = model(data_sample.view((1,) + tuple(data_sample.shape)))
+    data_sample, norm_params = next(iter(validation_dataloader))
+    # reconstructed = model(data_sample.view((1,) + tuple(data_sample.shape)))
     trainer.model.eval()
     reconstructed = model(data_sample)
 
@@ -179,16 +202,18 @@ if __name__ == "__main__":
 
     data = data_sample.cpu().detach().numpy()
     reconstructed = reconstructed.cpu().detach().numpy()
-    plotting.plot_histogram(
-        data[:, 0, :, :, :], reconstructed[:, 0, :, :, :], title='x', bins=20)
-    plt.savefig('plots/x_hist.png')
-    plotting.plot_histogram(
-        data[:, 1, :, :, :], reconstructed[:, 1, :, :, :], title='y', bins=20)
-    plt.savefig('plots/y_hist.png')
-    plotting.plot_histogram(
-        data[:, 2, :, :, :], reconstructed[:, 2, :, :, :], title='up', bins=20)
-    plt.savefig('plots/up_hist.png')
-    plotting.plot_contour(data, reconstructed, title='x')
-    plt.savefig('plots/contour_hist.png')
-    plt.show(block=False)
-    input("Press key to exit")
+    plotting.plot_arrows3D(data[0, :], reconstructed[0, :], 5)
+    plt.show()
+    # plotting.plot_histogram(
+    #     data[:, 0, :, :, :], reconstructed[:, 0, :, :, :], title='x', bins=20)
+    # plt.savefig('plots/x_hist.png')
+    # plotting.plot_histogram(
+    #     data[:, 1, :, :, :], reconstructed[:, 1, :, :, :], title='y', bins=20)
+    # plt.savefig('plots/y_hist.png')
+    # plotting.plot_histogram(
+    #     data[:, 2, :, :, :], reconstructed[:, 2, :, :, :], title='up', bins=20)
+    # plt.savefig('plots/up_hist.png')
+    # plotting.plot_contour(data, reconstructed, title='x')
+    # plt.savefig('plots/contour_hist.png')
+    # plt.show(block=False)
+    # input("Press key to exit")
