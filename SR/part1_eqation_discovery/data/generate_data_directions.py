1f498f3cf282c176f4033bcb11ae99537e4b072c
Gustav <g.often94@gmail.com>
2020-04-01 13:03:36 +0200

    linux support

--- SR/part1_eqation_discovery/data/generate_data_directions.py
+++ SR/part1_eqation_discovery/data/generate_data_directions.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from Interpolering import interpolation
+import platform
 #dx = 221
 dx = 221
 dy = 221
